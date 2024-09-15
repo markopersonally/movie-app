ade1158fdacbd56acd2251d2cea5708abe9aac01
mkpage1337 <mrku1337@gmail.com>
2024-09-15 22:06:35 +0200

    new navigation styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useAuth } from "../contexts/authContext/authContext";
 import { doSignOut } from "../firebase/auth";
 import { Button } from "@mui/material";
@@ -8,17 +8,27 @@ export function Header() {
 
   const ulStyle = "text-xl flex gap-5 justify-center items-center";
 
+  const link = "text-black hover:text-violet-300 delay-100";
+  const activLink = `${link} text-violet-500`;
+
+  const linkStyle = ({ isActive }: { isActive: boolean }) =>
+    isActive ? activLink : link;
+
   return (
     <header>
       <nav>
         <ul className={ulStyle}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink className={linkStyle} to="/">
+              Home
+            </NavLink>
           </li>
           {userLoggedIn ? (
             <>
               <li>
-                <Link to="/movies">Movies</Link>
+                <NavLink className={linkStyle} to="/movies">
+                  Movies
+                </NavLink>
               </li>
               <li>
                 <Button onClick={doSignOut} color="secondary">
@@ -29,10 +39,14 @@ export function Header() {
           ) : (
             <>
               <li>
-                <Link to="/login">Login</Link>
+                <NavLink className={linkStyle} to="/login">
+                  Login
+                </NavLink>
               </li>
               <li>
-                <Link to="/register">Register</Link>
+                <NavLink className={linkStyle} to="/register">
+                  Register
+                </NavLink>
               </li>
             </>
           )}
