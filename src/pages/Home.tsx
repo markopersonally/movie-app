b80580ff837fe239f0f532c241590eb5421a7969
mkpage1337 <mrku1337@gmail.com>
2024-09-17 22:32:25 +0200

    style/home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { apiKey, popular } from "../data/api";
+
+interface MovieList {
+  id: number;
+  title: string;
+  poster_path: string;
+  release_date: string;
+  backdrop_path: string;
+}
+
 export function Home() {
+  const [movieList, setMovieList] = useState<MovieList[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios.get(`${popular}?api_key=${apiKey}`).then((response) => {
+      const result = response.data.results;
+      setMovieList(result);
+      console.log(result);
+    });
+  };
+
   return (
-    <div className="bg-[url('./images/main-background.jpg')] absolute top-0 left-0 w-screen h-screen blur z-[-5]"></div>
+    <main className="max-[1400px]">
+      <div className="bg-[url('./images/main-background.jpg')] absolute top-0 left-0 w-screen h-screen blur z-[-5]"></div>
+      <div className="mt-10 flex flex-wrap gap-5 justify-center items-center">
+        {movieList.map((film) => (
+          <div key={film.id}>
+            {film.poster_path && (
+              <img
+                className="hover:scale-[1.1]"
+                src={`https://image.tmdb.org/t/p/w200${film.backdrop_path}`}
+                alt={`${film.title}`}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
