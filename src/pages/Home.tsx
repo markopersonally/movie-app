7c18244815f8d45c34ff39116866fd2b27b27d1d
mkpage1337 <mrku1337@gmail.com>
2024-09-17 20:50:22 +0200

    style/home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 export function Home() {
   return (
-    <div>
-      
-    </div>
+    <div className="bg-[url('./images/main-background.jpg')] absolute top-0 left-0 w-screen h-screen blur z-[-5]"></div>
   );
 }
